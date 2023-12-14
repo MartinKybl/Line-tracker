17217907cbd0fd200caf5ede96bce6c4d353e6ee
matoushep <hepmatous@seznam.cz>
2023-12-14 14:52:28 +0100

    čachry machry s teplotou

--- dopravni_nehoda.py
+++ dopravni_nehoda.py
@@ -1,19 +1,23 @@
 from machine import Pin
 import time
+import math
 
 Trig = Pin(0, Pin.OUT)
 Echo = Pin(1, Pin.IN, Pin.PULL_DOWN)
 
-Diff=0
-while True:
-    tTrig=time.ticks_ms()
+t=20
+v=331*math.sqrt(1+t/273.15)
+Stop=5
+Diff=200
+while Diff>Stop:
+    tTrig=time.ticks_us()
     if Echo.value()==0:
         Trig.on()
-        time.sleep_ms(1000)
+        time.sleep_us(20)
         print("vyslano")
     elif Echo.value()==1:
         Trig.off()
-        tEcho=time.ticks_ms()
+        tEcho=time.ticks_us()
         Diff=time.ticks_diff(tEcho,tTrig)
         print("ceska posta")
         print(Diff)
