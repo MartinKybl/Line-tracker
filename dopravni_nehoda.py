bde6fdfd68f22ce8eb28153bcee15dbed6e39e3f
MatousHep <hepmatous@seznam.cz>
2024-02-22 17:47:16 +0100

    Ted uz to maka jak cerny na plantazich

--- dopravni_nehoda.py
+++ dopravni_nehoda.py
@@ -1,23 +1,22 @@
-from machine import Pin
+from machine import Pin, time_pulse_us
 import time
-import math
+#import math
 
 Trig = Pin(0, Pin.OUT)
 Echo = Pin(1, Pin.IN, Pin.PULL_DOWN)
 
-t=20
-v=331*math.sqrt(1+t/273.15)
-Stop=5
-Diff=200
-while Diff>Stop:
-    tTrig=time.ticks_us()
-    if Echo.value()==0:
-        Trig.on()
-        time.sleep_us(20)
-        print("vyslano")
-    elif Echo.value()==1:
-        Trig.off()
-        tEcho=time.ticks_us()
-        Diff=time.ticks_diff(tEcho,tTrig)
-        print("ceska posta")
-        print(Diff)
+#t=20000
+#v=331*math.sqrt(1+t/273.15)
+Stop = 69
+Jarda_jede = True
+while True:
+    Trig.value(0)
+    time.sleep_us(5)
+    Trig.value(1)
+    time.sleep_us(10)
+    Trig.value(0)
+    Bouracka = time_pulse_us(Echo, 1, 30000)
+    if Bouracka < Stop:
+        Jarda_jede = False
+    else:
+        Jarda_jede = True
